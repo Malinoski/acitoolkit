0d3ee88928f2d1746de740f670dea1f27f6e5702
malinoski <iuri.malinoski@g.globo>
2022-01-13 16:08:53 -0300

    Code update

--- my-samples/aci-find-candidate-interfaces-to-epg-attement.py
+++ my-samples/aci-find-candidate-interfaces-to-epg-attement.py
@@ -2,8 +2,8 @@ from typing import List
 
 from acitoolkit import Session, Pod, Interface, Node, Linecard
 
-session = Session("https://sandboxapicdc.cisco.com", "admin", "!v3G@!4@Y")
-# session = Session("https://10.10.20.14", "admin", "C1sco12345")
+# session = Session("https://sandboxapicdc.cisco.com", "admin", "!v3G@!4@Y")
+session = Session("https://10.10.20.14", "admin", "C1sco12345")
 resp = session.login()
 if not resp.ok:
     print('%% Could not login to APIC')
@@ -11,11 +11,11 @@ if not resp.ok:
 # Pods
 pods: List[Pod] = Pod.get(session)
 for pod in pods:
-
+    print('\nPOD:{}'.format(pod.name))
     # Nodes
     nodes: List[Node] = Node.get(session, pod)
     for node in nodes:
-
+        print('\nNODE:{}'.format(node.name))
         # Filter
         if node.role == "leaf":
 
@@ -26,7 +26,7 @@ for pod in pods:
                 # Interfaces
                 interfaces: List[Interface] = Interface.get(session, linecard)
                 for interface in interfaces:
-
+                    print('\nINTERFACE:{}'.format(interface.name))
                     if 'discovery' in interface.attributes['usage']:
 
-                        print("\n{}".format(interface.attributes))
+                        print("AVAILABLE: {}".format(interface.attributes))

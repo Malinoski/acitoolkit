2dc8cea6c3d6b6af278235d89d5b62825f7b0ea8
malinoski <iuri.malinoski@g.globo>
2022-01-25 16:16:21 -0300

    Code update

--- my-samples/aci-attach-epg-to-interface.py
+++ my-samples/aci-attach-epg-to-interface.py
@@ -1,21 +1,21 @@
-from acitoolkit import Session, Tenant, AppProfile, EPG, Interface, L2Interface, BridgeDomain
+from acitoolkit import Session, Tenant, AppProfile, EPG, Interface, L2Interface, BridgeDomain, Node
 
-# session = Session("https://sandboxapicdc.cisco.com", "admin", "!v3G@!4@Y")
-session = Session("https://10.10.20.14", "admin", "C1sco12345")
+session = Session("https://sandboxapicdc.cisco.com", "admin", "!v3G@!4@Y")
+# session = Session("https://10.10.20.14", "admin", "C1sco12345")
 
 resp = session.login()
 if not resp.ok:
     print('%% Could not login to APIC')
 
 # Create the Tenant, App Profile, and EPG
-tenant = Tenant('tenantA')
-app = AppProfile('apA', tenant)
-bd = BridgeDomain('bdA', tenant)
-epg = EPG('epgA', app)
-epg.add_bd(bd)
+tenant = Tenant('tenant_public')
+app = AppProfile('ap1_public', tenant)
+# bd = BridgeDomain('db_public', tenant)
+epg = EPG('my_epg1', app)
+# epg.add_bd(bd)
 
 # Get interface (p.s.: node can bve a leaf, and module is a linecard)
-intf = Interface(interface_type='eth', pod='1', node='101', module='1', port='1')
+intf = Interface(interface_type='eth', pod='1', node='101', module='1', port='2')
 
 # Set a free VLAN, including mode access 802.1P (encap_mode='native') and encapsulation mode 5 (encap_id='5')
 free_vlan = "101"  # TODO: Get a free VLAN in APIC (or in NetworkAPI/environments).
@@ -30,6 +30,7 @@ vlan_intf.attach(intf)
 
 # Attach the Interface and VLAN configuration to EPG
 epg.attach(vlan_intf)
+epg.set_deployment_immediacy("immediate")
 
 # Push configuration to the APIC
 resp = tenant.push_to_apic(session)

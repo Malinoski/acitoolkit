f49444d5422f8ca9d27b87e90db40431a4ccb1ee
malinoski <iuri.malinoski@g.globo>
2022-02-16 11:08:09 -0300

    Including tree view

--- my-samples/aci-show-interfaces.py
+++ my-samples/aci-show-interfaces.py
@@ -1,32 +1,49 @@
-import json
-import sys
-import acitoolkit.acitoolkit as ACI
+from pprint import pprint
+from typing import List
+from requests.models import Response
+from acitoolkit import Session, Pod, Interface, Node, Linecard, VmmDomain
 
-# session = ACI.Session("https://sandboxapicdc.cisco.com", "admin", "!v3G@!4@Y")
-session = ACI.Session("https://10.10.20.14", "admin", "C1sco12345")
-resp = session.login()
+session = Session("https://sandboxapicdc.cisco.com", "admin", "!v3G@!4@Y")
+# session = Session("https://10.10.20.14", "admin", "C1sco12345")
+
+resp: Response = session.login()
 if not resp.ok:
     print('%% Could not login to APIC')
-    sys.exit(0)
 
-# Download all of the interfaces
-data = []
-interfaces = ACI.Interface.get(session)
-for interface in interfaces:
-    print("{}".format(json.dumps(interface.attributes, indent=4)))
-    data.append((interface.attributes['if_name'],
-                 interface.attributes['porttype'],
-                 interface.attributes['adminstatus'],
-                 interface.attributes['operSt'],
-                 interface.attributes['operSpeed'],
-                 interface.attributes['mtu'],
-                 interface.attributes['usage']))
+# Pods
+pods: List[Pod] = Pod.get(session)
+for pod in pods:
+    print('# POD:')
+    print('name: {}'.format(pod.name))
 
-# Display the some data as table
-template = "{0:17} {1:6} {2:^6} {3:^6} {4:7} {5:6} {6:9} "
-print(template.format("INTERFACE", "TYPE", "ADMIN", "OPER", "SPEED", "MTU", "USAGE"))
-print(template.format("---------", "----", "------", "------", "-----", "___", "---------"))
-for rec in data:
-    print(template.format(*rec))
+    # Nodes
+    nodes: List[Node] = Node.get(session, pod)
+    for node in nodes:
 
+        # Filter
+        if node.role == "leaf":
+            print('\t# LEAF')
+            print('\tname: {}'.format(node.name))
 
+            # Linecards
+            linecards: List[Linecard] = Linecard.get(session, node)
+            for linecard in linecards:
+
+                # Interfaces
+                interfaces: List[Interface] = Interface.get(session, linecard)
+                for interface in interfaces:
+                    print('\t\t# INTERFACE')
+                    print('\t\tname: {}'.format(interface.name))
+                    print('\t\tid: {}'.format(interface.id))
+                    print('\t\tdn: {}'.format(interface.dn))
+                    print('\t\tpod: {}'.format(interface.pod))
+                    print('\t\tport: {}'.format(interface.port))
+                    print('\t\tmodule: {}'.format(interface.module))
+                    print('\t\tnode: {}'.format(interface.node))
+                    print('\t\ttype: {}'.format(interface.type))
+                    print('\t\tdescr: {}'.format(interface.descr))
+                    print('\t\tif_name: {}'.format(interface.if_name))
+                    print('\t\tadminstatus: {}'.format(interface.adminstatus))
+                    print('\t\tinterface_type: {}'.format(interface.interface_type))
+                    print('\t\tjson {}'.format(interface.get_json()))
+                    print('\t\tattributes: {}'.format(interface.attributes))
